a7abe790f7d7dee65961665172262d6b840f6fbb
ElijahBond <bondarefil@gmail.com>
2023-06-27 22:35:41 +0300

    done adaptive for itemPage

--- src/components/ItemPage/ItemPage.tsx
+++ src/components/ItemPage/ItemPage.tsx
@@ -1,17 +1,33 @@
-import { ItemDescription } from './ItemDescription';
 import itemPageStyles from './ItemPage.module.scss';
+
+import itemInfoStyles from '../items/itemInfo.module.scss';
+import { nb574, shoppingCart } from '../../assets';
 
 const ItemPage = () => {
 
     return (
-        <div className={itemPageStyles.main}>
-            <button 
-                className={itemPageStyles.button}
-            >
-                Back in catalog
-            </button>
+        <div className={itemPageStyles.wrapper}>
+            <div className={itemPageStyles.main}>
+                <button 
+                    className={itemPageStyles.button}
+                >
+                    Back in catalog
+                </button>
 
-            <ItemDescription />
+                <img 
+                    className={itemPageStyles.itemImg}
+                    src={nb574} />
+
+                <p className={itemPageStyles.description}>New Balance 574 Vintage Brights</p>
+                <div className={itemPageStyles.model_number}>Item model number: MT91547</div>
+
+                <div>
+                    <div className={itemInfoStyles.cart_button}>
+                        <img src={shoppingCart} />
+                    </div>
+                    <span>$ 650</span>
+                </div>
+            </div>
         </div>
     )
 }
