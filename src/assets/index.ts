18264dbd059d23a55e8b60c65cdc799b5ce511bb
ElijahBond <bondarefil@gmail.com>
2023-07-06 22:52:53 +0300

    replace svg

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,15 +3,6 @@ import shopImage from './SP.svg';
 import shoppingCart from './shoppingCart.svg';
 import burgerButton from './burger.svg';
 
-// items images
-import nbRacer from './itemsImg/nb-racer.svg';
-import nb373 from './itemsImg/nb373.svg';
-import nb574 from './itemsImg/nb574.svg';
-import nb670 from './itemsImg/nb670.svg';
-import nb920 from './itemsImg/nb920.svg';
-import nb5740 from './itemsImg/nb5740.svg';
-
-
 // basket images
 import clearImg from './basketImg/clear.svg';
 import decrementImg from './basketImg/decrement.svg';
@@ -23,13 +14,6 @@ export {
     shopImage,
     shoppingCart,
     burgerButton,
-
-    nbRacer,
-    nb373,
-    nb574,
-    nb670,
-    nb920,
-    nb5740,
 
     clearImg,
     decrementImg,
